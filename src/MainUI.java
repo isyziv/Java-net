4223cd368c7388d6a97df5608abc1b35abf23406
xu <a7771471@gmail.com>
2023-10-18 13:58:37 +0800

    add allow multiple receiver

--- src/MainUI.java
+++ src/MainUI.java
@@ -15,6 +15,9 @@ public class MainUI extends JFrame{
     private JPasswordField password;
     private JTextField port;
     private JPanel mainPanel;
+    public boolean containsKeyword(String text, String keyword) {
+        return text.indexOf(keyword) != -1;
+    }
     public MainUI(){
         setContentPane(mainPanel);
         setTitle("main");
@@ -24,18 +27,23 @@ public class MainUI extends JFrame{
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(ip.getText());
-                System.out.println(port.getText());
-                System.out.println(from.getText());
-                System.out.println(account.getText());
-                String passwordString = new String(password.getPassword());
-                System.out.println(passwordString);
-                System.out.println(to.getText());
-                System.out.println(subject.getText());
-                System.out.println(message.getText());
-                MailSender mailSender = new MailSender();
-                mailSender.sender(ip.getText(),Integer.parseInt(port.getText()),from.getText(),passwordString,account.getText(),to.getText(),subject.getText(),message.getText());
-                JOptionPane.showMessageDialog(null, "Mail Send.", "Success", JOptionPane.INFORMATION_MESSAGE);
+                String toSplit =to.getText();
+                String[] parts = toSplit.split(",");
+                int i = 0,j= 0;
+                for(String part : parts) {
+                    j++;
+                    if (containsKeyword(part, "@")) {
+                        String passwordString = new String(password.getPassword());
+                        MailSender mailSender = new MailSender();
+                        mailSender.sender(ip.getText(),Integer.parseInt(port.getText()),from.getText(),passwordString,account.getText(),part,subject.getText(),message.getText());
+                        i++;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Email address is illegal", "Error", JOptionPane.ERROR_MESSAGE);
+
+                    }
+
+                }
+                JOptionPane.showMessageDialog(null, "Mail Send.\r\nSend to"+toSplit+"\r\nWW計出"+i+"封", "Success", JOptionPane.INFORMATION_MESSAGE);
             }
         });
         quitButton.addActionListener(new ActionListener() {
