77f731952621930a0bde90a7a90f29bb70c8f924
xu <a7771471@gmail.com>
2023-10-28 02:46:48 +0800

    add http1.0

---
+++ http1/src/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        int port = 8080;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                executorService.execute(new ClientHandler(socket));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket socket;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+
+                String line;
+                String requestMethod = "";
+                String requestBody = "";
+                StringBuilder body = new StringBuilder();
+                StringBuilder head = new StringBuilder();
+
+                while ((line = in.readLine()) != null) {
+                    head.append("\r\n").append(line); // Build the HTTP headers
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                    if (requestMethod.isEmpty()) {
+                        requestMethod = line.split(" ")[0];
+                    }
+                }
+
+                if (requestMethod.equals("POST")) {
+                    while (in.ready()) {
+                        body.append((char) in.read());
+                    }
+                    requestBody = body.toString();
+                }
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+                String currentTime = dateFormat.format(new Date());
+
+                out.println("HTTP/1.0 200 OK");
+                out.println("Content-Type: text/html");
+                out.println("Connection: close");
+                out.println("Timestamp: " + currentTime);
+                out.println();
+
+                if (requestMethod.equals("GET") || requestMethod.equals("POST")) {
+                    out.println("<html><body><h1>Hello, World!</h1></body>");
+                    out.println("<p>Request Head: <br><pre>" + head + "</pre></p>");
+                    if (!requestBody.isEmpty()) {
+                        out.println("<p>Request Body: <br><pre>" + requestBody + "</pre></p>");
+                    }
+                    out.println("</html>");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
