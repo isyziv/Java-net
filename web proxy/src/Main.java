39ef70ba54a6f2a068470292cab13930c96f1f17
xu <42688005+isyziv@users.noreply.github.com>
2023-11-15 15:09:53 +0800

    .

--- web proxy/src/Main.java
+++ web proxy/src/Main.java
@@ -1,61 +1,193 @@
-以下是對你的程式碼的解讀及相關的說明：
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-```plaintext
-# 程式碼解讀及說明
+public class Main {
+    public static void main(String[] args) {
+        int port = 8080;
 
-## 1. 主程式 (Main class)
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-```java
-public static void main(String[] args)
-```
-- 這是程式的進入點，主要功能是建立一個伺服器套接字 (ServerSocket) 並等待客戶端連接。
-- 使用 ExecutorService 來管理執行緒池，每當有新的客戶端連接，就會提交一個執行緒給執行緒池處理。
+            while (true) {
+                Socket socket = serverSocket.accept();
+                executorService.execute(new ClientHandler(socket));
+            }
 
-```java
-try (ServerSocket serverSocket = new ServerSocket(port)) {
-    // ...
-    while (true) {
-        Socket socket = serverSocket.accept();
-        executorService.execute(new ClientHandler(socket));
-    }
-} catch (IOException e) {
-    e.printStackTrace();
-}
-```
-- 建立 ServerSocket 並指定監聽的埠號 (port)。
-- 使用 try-with-resources 確保 ServerSocket 在使用完畢後能夠正確關閉。
-- 進入無窮迴圈，等待客戶端連接，每當有新的連接，就創建一個 `ClientHandler` 實例，交由執行緒池處理。
-
-## 2. 客戶端處理 (ClientHandler class)
-
-```java
-static class ClientHandler implements Runnable {
-    // ...
-    public void run() {
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-            // ...  // 客戶端處理邏輯
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
-    // ...
-}
-```
-- `ClientHandler` 實現了 Runnable 介面，負責處理每個客戶端的請求。
-- 使用 BufferedReader 讀取客戶端的輸入流，並使用 PrintWriter 寫入回應。
-- 解析 HTTP 請求中的方法、參數等資訊，並根據 `SendRequest` 方法向指定的 URL 發送請求。
-- 最後，確保關閉與客戶端的連接。
 
-```java
-public static String SendRequest(String url, String requestMethod) throws IOException {
-    // ...  // 向指定 URL 發送請求的邏輯
+    static class ClientHandler implements Runnable {
+        private Socket socket;
+        public static Map<String, String> stringMap = new HashMap<>();
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+
+                String line;
+                String params = "";
+                String path = "";
+                String requestMethod = "";
+                String requestBody = "";
+                StringBuilder body = new StringBuilder();
+                StringBuilder head = new StringBuilder();
+                String url = "";
+                String resp = "";
+                while ((line = in.readLine()) != null) {
+                    head.append("\r\n").append(line); // Build the HTTP headers
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                    if (requestMethod.isEmpty()) {
+                        requestMethod = line.split(" ")[0];
+                        if (line.contains("?")) {
+                            String value = getParameterValue(line, "url");
+                            url = value;
+                            System.out.println("URL參數值: " + value);
+                            params = line.split("\\?")[1];
+                            params = params.split(" ")[0];
+                            path = line.split("\\?")[0];
+                            path = path.split(" ")[1];
+                        }
+                        else {
+                            path = line.split(" ")[1];
+                            //path = path.split("/")[1];
+                        }
+                    }
+                }
+
+                if (requestMethod.equals("POST")) {
+                    while (in.ready()) {
+                        body.append((char) in.read());
+                    }
+                    requestBody = body.toString();
+                }
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+                String currentTime = dateFormat.format(new Date());
+
+                out.println("HTTP/1.0 200 OK");
+                out.println("Content-Type: text/html");
+                out.println("Connection: Keep-Alive");
+                //out.println("Connection: close");
+                out.println("Timestamp: " + currentTime);
+                out.println();
+                params = params.replace("&", "\r\n");
+                if (requestMethod.equals("GET")) {
+                    if (stringMap.containsKey(url))
+                    {
+                        System.out.println("use cache");
+                        resp = stringMap.get(url);
+                    }
+                    else {
+                        System.out.println("get page");
+                        resp = get_http(url);
+                        stringMap.put(url, resp);
+                    }
+                    out.println(resp);
+                }
+                else {
+                    out.println("<html><body><h1>Method not allowed!</h1></body></html>");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        private static String getParameterValue(String url, String parameterName) {
+            // 找到 "?" 的位置
+            int questionMarkIndex = url.indexOf("?");
+
+            // 如果沒有問號或者參數名不存在，返回 null
+            if (questionMarkIndex == -1 || !url.contains(parameterName + "=")) {
+                return null;
+            }
+
+            // 截取 "?" 之後的字符串
+            String queryString = url.substring(questionMarkIndex + 1);
+
+            // 將查詢字符串分割成鍵值對
+            String[] pairs = queryString.split("&");
+            for (String pair : pairs) {
+                // 分割鍵值對
+                String[] keyValue = pair.split("=");
+
+                // 檢查參數名是否匹配
+                if (keyValue.length == 2 && keyValue[0].equals(parameterName)) {
+                    // 返回參數值
+                    return keyValue[1];
+                }
+            }
+
+            // 如果參數不存在，返回 null
+            return null;
+        }
+
+        public static String get_http(String url) {
+            // 使用者輸入的網址
+            //String url = "http://httpbin.org/";
+
+            try {
+                String response = sendGetRequest(url);
+                return response;
+            } catch (IOException e) {
+                return "error";
+            }
+        }
+
+        private static String getUserInput(String prompt) {
+            System.out.print(prompt);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException("讀取使用者輸入時發生錯誤: " + e.getMessage());
+            }
+        }
+
+        private static String sendGetRequest(String url) throws IOException {
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            // 設定請求類型為GET
+            connection.setRequestMethod("GET");
+
+            // 取得回應碼
+            int responseCode = connection.getResponseCode();
+
+            // 讀取回應內容
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            reader.close();
+
+            return response.toString();
+        }
+    }
 }
-```
-- `SendRequest` 方法用於向指定 URL 發送 HTTP 請求，並讀取回應內容。
